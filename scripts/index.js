e8d946d85e5018edc73a3b83603339d889fcf81c
Michael Alikbarov <michael.alikbarov@yandex.ru>
2024-04-29 22:35:33 +0500

    выполнить ПР-5

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
+const cardsList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+const createCard = (name, link, callback) => {
+    const cardItem = cardTemplate.cloneNode(true);
+    cardItem.querySelector('.card__title').textContent = name;
+    cardItem.querySelector('.card__image').setAttribute('src', link);
+    cardItem.querySelector('.card__image').setAttribute('alt', name);
+    const deleteButton = cardItem.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', callback);
+    cardsList.append(cardItem);
+}
 
-// @todo: Функция удаления карточки
+function removeCard(evt) {
+    const cardElement = evt.target.closest('.card');
+    cardElement.remove();
+}
 
-// @todo: Вывести карточки на страницу
+function withdrawCards(cards) {
+    cards.forEach((data) => {
+        createCard(data.name, data.link, removeCard);
+    })
+};
+
+withdrawCards(initialCards);
